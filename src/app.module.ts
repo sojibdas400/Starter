56e733a5c01e93de84875f38ee520f81281cd07f
sojibdas400 <sojibdas400@gmail.com>
2021-11-23 00:44:51 +0600

    Postgres connect in nestJS

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import moduleName, { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AllExceptionFilter } from './common/filters';
 import { ResponseTransformInterceptor } from './common/interceptors';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+      cache: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) =>
+        configService.get('database'),
+    }),
+  ],
   controllers: [AppController],
   providers: [
     {
